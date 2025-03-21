dca4e0052f60bf7fc7ef963f8ed89d2829c9df4a
Onin Sibua <sibuanino@gmail.com>
2025-03-21 14:27:54 +0800

    Updated Javascript file with codes from Mikee's

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ let gameBoard = [
   ["", "", ""],
 ];
 
+let state = [];
 let moves = 0;
 let playerTurn1 = true; // True = X, False = O
 
@@ -16,7 +17,6 @@ let playerTurn1 = true; // True = X, False = O
 for (let i = 0; i < cells.length; i++) {
   cells[i].addEventListener("click", function () {
     addMove(cells[i], i);
-    // will add .push(i) here to update gameBoard?
   });
 }
 
@@ -35,4 +35,54 @@ function addMove(element, boxNumber) {
       playerTurn1 = true;
     }
   }
+
+  updateBoard(specificGrid, boxNumber);
 }
+
+function updateBoard(element, boxNumber) {
+  let row = Math.floor(boxNumber / 3);
+  let column = boxNumber % 3;
+  gameBoard[row][column] = element.innerText;
+
+  updateState(gameBoard);
+}
+
+function updateState(boardCopy) {
+  const newBoard = [];
+  for (let i = 0; i < boardCopy.length; i++) {
+    const row = [];
+    for (let j = 0; j < boardCopy[i].length; j++) {
+      row.push(boardCopy[i][j]);
+    }
+
+    newBoard.push(row);
+  }
+
+  state.push(newBoard);
+  console.log(state);
+
+  checkEndGame();
+}
+
+function checkEndGame() {
+  // check winning combination
+  if (moves === 9) {
+    prev.classList.add("show");
+  }
+}
+
+function reflectBoard(index) {
+  let tempBoard = state[index];
+  let moveString = [];
+  for (let i = 0; i < tempBoard.length; i++) {
+    for (let j = 0; j < tempBoard[i].length; j++) {
+      moveString.push(tempBoard[i][j]);
+    }
+  }
+
+  for (let grid = 0; grid < moveString.length; grid++) {
+    document.getElementById(`box${grid}`).textContent = moveString[grid];
+  }
+}
+
+prev.addEventListener("click", () => reflectBoard(1));
